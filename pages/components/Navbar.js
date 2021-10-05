e8cb6924248054bfaa210e95e13c69b50f1e1f51
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-10-05 17:27:23 +0530

    added the components folder

---
+++ pages/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from "styled-components"
+import ".././../styles/home.module.css"
+
+function Navbar() {
+    return (
+        <div>
+
+        <Container>
+            hello
+        </Container>
+            
+        </div>
+    )
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: red;
+  padding: 0;
+  box-sizing: border-box;
+`;
+
+
+
+
+export default Navbar
+
+
+
